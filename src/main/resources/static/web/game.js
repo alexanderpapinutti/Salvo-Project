3574cf34db85e4b5664eb8dbfb483673870787cb
AceVentura <alexpapinutti@gmail.com>
2018-12-06 12:12:54 +0100

    Implementing Salvoes

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -1,83 +1,75 @@
-$(function () {
+const mainData = {
+    userShips: [],
+    userSalvos: [],
+    columnHeaders: ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+    rowHeaders: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+    player1: '',
+    player2: '',
+}
 
-    const columnHeaders = ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-    const rowHeaders = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+loadGamePlayerData();
+
+
+
+function loadGamePlayerData() {
+
+    $("#tableContainer").append(generateGrid("U", mainData.columnHeaders, mainData.rowHeaders));
+    getData();  
+    console.log(mainData.player1)
     
-    function indexCompare (){
-        
-    }
-    
-    function loadGamePlayerData() {
-        var urlParams = new URLSearchParams(window.location.search);
+}
 
-        var entries = urlParams.entries();
-        for (player of entries) {
-            $.get("/api/game_view/" + player[1])
-                .done(function (data) {
-                    $.getJSON("/api/game_view/" + player[1], function (data) {
-                        var numberOfShips = data.userShips.length;
-                        var shipsTypes = [];
-                        var shipsLocations = [];
-                        for (var i = 0; i < numberOfShips; i++) {
-                            shipsTypes.push(data.userShips[i].type)
-                            for (var j = 0; j < 5; j++) {
-                                if (data.userShips[i].locations[j] != null) {
-                                    shipsLocations.push(data.userShips[i].locations[j]);
-                                }
-
-                            }
-
-                        }
-                        
-                        console.log(shipsLocations);
-
-                        function generateGrid(rows, cols) {
-                            var grid = "<table>";
-                            for (row = 0; row <= rows; row++) {
-                                grid += "<tr>";
-                                var indexRow = $("tr").index(this) + row + 2;
-                                for (col = 0; col <= cols; col++) {
-                                    var indexColumn = $("td").index(this) + col + 2;
-                                    if (indexRow == 1) {
-                                        grid += "<td class='myColumnHeaders'>" + columnHeaders[col] + "</td>";
-                                    } else if (indexColumn == 1) {
-                                        grid += "<td class='myRowHeaders'>" + rowHeaders[row - 1] + "</td>"
-                                    } else {
-                                        grid += "<td class='emptyCells'>"+ rowHeaders[row - 1]+ columnHeaders[col] + "</td>";
-                                    
-                                    }
-                                }
-                                grid += "</tr>";
-
-                            }
-
-                            return grid;
-                        }
-
-                        $("#tableContainer").append(generateGrid(10, 10));
-                        
-                        $("td").click(function () {
-                            $("td").css('background-color', 'white');
-                            var index = $("td").index(this);
-                            var row = Math.floor((index) / 11) - 1;
-                            
-                            var col = (index % 11);
-                            console.log(col,row);
-//                            console.log(rowHeaders[row]);
-                            
-                            $("span").text("That was row " + rowHeaders[row] + " and col " + col);
-                            $(this).css('background-color', 'red');
-
-                        });
-                    });
-                })
-                .fail(function (jqXHR, textStatus) {
-                    console.log("Failed: " + textStatus);
-                });
+function generateGrid(tableId,columnHeaders, rowHeaders) {
+    let rows = rowHeaders.length;
+    let cols = columnHeaders.length;
+    var grid = "<table>";
+    for (row = 0; row <= rows; row++) {
+        grid += "<tr>";
+        var indexRow = $("tr").index(this) + row + 2;
+        for (col = 0; col < cols; col++) {
+            var indexColumn = $("td").index(this) + col + 2;
+            if (indexRow == 1) {
+                grid += "<td class='myColumnHeaders'>" + columnHeaders[col] + "</td>";
+            } else if (indexColumn == 1) {
+                grid += "<td class='myRowHeaders'>" + rowHeaders[row - 1] + "</td>"
+            } else {
+                grid += "<td class='emptyCells' id=" + tableId + rowHeaders[row - 1] + columnHeaders[col] + ">" + rowHeaders[row - 1] + columnHeaders[col] + "</td>";
+            }
         }
-        
+        grid += "</tr>";
+
     }
 
-    loadGamePlayerData();
+    return grid;
+}
 
-});
+function printShips(arrayOfShips) {
+    for (var i = 0; i < arrayOfShips.length; i++) {
+        let shipLocations = arrayOfShips[i].locations;
+        let shipType = arrayOfShips[i].type;
+        for (var j = 0; j < shipLocations.length; j++) {
+            let location = shipLocations[j];
+            $("#U" + location).addClass('ship-location ' + shipType);
+        }
+    }
+}
+
+function getData() {
+    $.getJSON("/api/game_view/" + location.search.split("=")[1], function (data) {
+        console.log(data)
+        mainData.userShips = data.userShips;
+        printShips(mainData.userShips);
+        mainData.player1 = data.game.gamePlayers[1].player.email;
+        mainData.player2 = data.game.gamePlayers[0].player.email;
+        $("#this-game-players").append(displayPlayers(mainData.player1, mainData.player2));
+           
+    })
+}
+
+function displayPlayers(player1, player2) {
+    console.log(player1, player2)
+    var display = "<p>";
+    display += player1 + "(you) vs. " + player2;
+    display += "</p>";
+    return display;
+}
